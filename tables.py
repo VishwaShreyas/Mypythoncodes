da2de28cf6ad027a63019c0f65e1e816a4a83d74
VishwaShreyas <120476385+VishwaShreyas@users.noreply.github.com>
2022-12-17 10:02:57 +0530

    Add files via upload

---
+++ tables.py
@@ -0,0 +1,4 @@
+a=int(input("The Multiplication Table of: " ))   
+n = int(input ("Enter the number of which the you want to print the multiplication table: ")) 
+for count in range(1,n+1 ):      
+   print (a, 'x', count, '=', a * count)    
